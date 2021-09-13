10ad337e0f2f5b6ff568d48c3b395cdd80aec089
SeoSuYoung <57447946+SeoSuYoung@users.noreply.github.com>
2021-09-13 19:21:11 +0900

    Create 서수영_2839.py
    3x + 5y = N(x는 3의 개수 y는 5의 개수)
    y = -3/5x + N/5,
    x, y 모두 1로 나누었을 때 나머지가 0이어야 함, 
    count에 횟수 저장, 최소값 출력(count가 비어있으면 -1출력)

---
+++ BACKJOON/Dynamic_Programming/2839/서수영_2839.py
@@ -0,0 +1,14 @@
+N = float(input())
+count = []
+
+#x(0 ~ N/3), y(-3/5x + N/5)
+for x in range(0, int(N/3)+1):
+    y = -3*x + N
+    #print(x, y)
+    if(x%1 == 0 and (y/5)%1 == 0):
+        count.append(x+(y/5))
+
+if len(count) != 0:
+    print(int(min(count)))
+else:
+    print(-1)
