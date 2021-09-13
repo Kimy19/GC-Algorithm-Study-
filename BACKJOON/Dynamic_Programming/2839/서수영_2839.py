c48a15ff0ad77542cd559b157ee85300097c2a9e
SeoSuYoung <57447946+SeoSuYoung@users.noreply.github.com>
2021-09-13 19:23:33 +0900

    Update 서수영_2839.py

--- BACKJOON/Dynamic_Programming/2839/서수영_2839.py
+++ BACKJOON/Dynamic_Programming/2839/서수영_2839.py
@@ -1,10 +1,10 @@
 N = float(input())
 count = []
 
-#x(0 ~ N/3), y(-3/5x + N/5)
+#x(0 ... N/3), y = -3/5x + N/5
 for x in range(0, int(N/3)+1):
     y = -3*x + N
-    #print(x, y)
+
     if(x%1 == 0 and (y/5)%1 == 0):
         count.append(x+(y/5))
 
